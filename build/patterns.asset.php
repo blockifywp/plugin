2c9d21efd1fed8ea31230a9bb155a8f550aba5da
seothemes <seothemeswp@gmail.com>
2022-12-20 08:27:43 +0800

    Add build dir

---
+++ build/patterns.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-i18n'), 'version' => '3c6589f634e456311a76');
---
+++ build/settings.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-edit-site', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'e3daa89cf3530a2dc433');
